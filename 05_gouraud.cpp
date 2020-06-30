9344fd8bbe86d5e23d3fa3d73e4e8e351f371fcd
rajvisavla <62500816+rajvisavla@users.noreply.github.com>
2020-06-30 13:43:47 +0530

    Add files via upload

---
+++ 05_gouraud.cpp
@@ -0,0 +1,613 @@
+/*
+  CSX75 Tutorial 5(a) Gouraud Shading
+
+  A program which draws a tesselated sphere 
+  and applies gouraud shading to it.
+  -------------------------------------
+  To run the code
+  ./05_shading <amount_of_tesselation>
+  -------------------------------------
+  
+  as long as amount of tesselation is between(20 & 360)
+   
+  Use the arrow keys and PgUp,PgDn,
+  keys to make the sphere move.
+  
+  Use the keys W, A, S, D to move Camera.
+
+  At starting the scene is in Perspective Mode, 
+  pressing P toggles the Wireframe.
+
+  Pressing G toggles different shading methods.
+
+  Modified from An Introduction to OpenGL Programming, 
+  Ed Angel and Dave Shreiner, SIGGRAPH 2013
+
+  Written by Aditya Prakash, 2015
+*/
+
+
+#include "05_gouraud.hpp"
+
+#include "filesystem.h"
+#include "shader_m.h"
+#include "camera.h"
+#include "model.h"
+#define STB_IMAGE_IMPLEMENTATION
+
+#include "stb_image.h"
+int tesselation=50; 
+bool wireframe=false;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+unsigned int loadCubemap(vector<std::string> faces);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// camera
+Camera camera(glm::vec3(0.0f, 0.0f, 0.0f));
+float lastX = (float)SCR_WIDTH / 2.0;
+float lastY = (float)SCR_HEIGHT / 2.0;
+bool firstMouse = true;
+
+// timing
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+
+
+double PI=3.14159265;
+GLuint shaderProgram;
+GLuint vbo[2], vao[2];
+
+glm::mat4 rotation_matrix;
+glm::mat4 projection_matrix;
+glm::mat4 c_rotation_matrix;
+glm::mat4 lookat_matrix;
+
+glm::mat4 model_matrix;
+glm::mat4 view_matrix;
+
+
+glm::mat4 modelview_matrix;
+glm::mat3 normal_matrix;
+
+GLuint uModelViewMatrix;
+GLuint viewMatrix;
+GLuint normalMatrix;
+
+
+
+
+const int num_vertices = 100000;
+
+int tri_idx=0;
+glm::vec4 v_positions[num_vertices];
+glm::vec4 v_colors[num_vertices];
+glm::vec4 v_normals[num_vertices];
+
+// Variables for wireframe
+int wire_idx=0;
+glm::vec4 w_positions[num_vertices];
+glm::vec4 w_colors[num_vertices];
+glm::vec4 w_normals[num_vertices];
+
+glm::vec4 color(0.6, 0.6, 0.6, 1.0);
+glm::vec4 black(0.2, 0.2, 0.2, 1.0);
+glm::vec4 white(1.0, 1.0, 1.0, 1.0);
+
+double Radius = 1;
+int Lat = 10;
+int Long = 10;
+
+void sphere(double radius, int Lats, int Longs)
+{
+  float lats,longs;
+
+  float slices=(180/(float(Lats)*10))/2;
+  float sectors=(180/(float(Longs)*10))/2;
+
+  float l;
+
+  for (lats = 0.0; lats <= PI; lats+=sectors)  
+  {
+      for(longs = 0.0; longs <= 2.0*PI; longs+=slices)
+	{
+	  float x = radius * sin(lats) * cos(longs);
+	  float y = radius * sin(lats) * sin(longs);
+	  float z = radius * cos(lats);
+	  glm::vec4 pt(x, y, z, 1.0);
+
+	  v_colors[tri_idx] = white; v_positions[tri_idx] = pt; 
+	  v_normals[tri_idx] = pt; tri_idx++;
+
+	  w_colors[wire_idx] = black; w_positions[wire_idx] = pt; 
+	  w_normals[wire_idx] = pt; wire_idx++;
+
+	  
+	  if(lats+sectors>PI)
+	    l=PI;
+	  else
+	    l=lats+sectors;
+	  x = radius * sin(l) * cos(longs);
+	  y = radius * sin(l) * sin(longs);
+	  z = radius * cos(l);
+	  pt =glm::vec4(x, y, z, 1.0);
+	  v_colors[tri_idx] = white; v_positions[tri_idx] = pt; 
+	  v_normals[tri_idx] = pt; tri_idx++;
+
+	  w_colors[wire_idx] = black; w_positions[wire_idx] = pt; 
+	  w_normals[wire_idx] = pt; wire_idx++;
+	  
+	}
+    }
+  // To Complete the wireframe
+  for (lats = 0.0; lats <= PI; lats+=sectors)  
+    {
+      for(longs = 0.0; longs <= 2.0*PI; longs+=slices)
+	{
+	  float x = radius * sin(lats) * cos(longs);
+	  float y = radius * sin(lats) * sin(longs);
+	  float z = radius * cos(lats);
+	  glm::vec4 pt(x, y, z, 1.0);
+	  
+	  w_colors[wire_idx] = black; w_positions[wire_idx] = pt; 
+	  w_normals[wire_idx] = pt; wire_idx++;
+	  
+	}
+    }
+}
+
+
+//-----------------------------------------------------------------
+
+void initBuffersGL(void)
+{
+
+  // Load shaders and use the resulting shader program
+  std::string vertex_shader_file("05_vshader.glsl");
+  std::string fragment_shader_file("05_fshader.glsl");
+
+  std::vector<GLuint> shaderList;
+  shaderList.push_back(csX75::LoadShaderGL(GL_VERTEX_SHADER, vertex_shader_file));
+  shaderList.push_back(csX75::LoadShaderGL(GL_FRAGMENT_SHADER, fragment_shader_file));
+
+  shaderProgram = csX75::CreateProgramGL(shaderList);
+  glUseProgram( shaderProgram );
+
+  // getting the attributes from the shader program
+  GLuint vPosition = glGetAttribLocation( shaderProgram, "vPosition" );
+  GLuint vColor = glGetAttribLocation( shaderProgram, "vColor" ); 
+  GLuint vNormal = glGetAttribLocation( shaderProgram, "vNormal" ); 
+  uModelViewMatrix = glGetUniformLocation( shaderProgram, "uModelViewMatrix");
+  normalMatrix =  glGetUniformLocation( shaderProgram, "normalMatrix");
+  viewMatrix = glGetUniformLocation( shaderProgram, "viewMatrix");
+
+  //Ask GL for two Vertex Attribute Objects (vao) , one for the colorcube and one for the plane.
+  glGenVertexArrays (2, vao);
+  //Ask GL for two Vertex Buffer Object (vbo)
+  glGenBuffers (2, vbo);
+
+  //Set 0 as the current array to be used by binding it
+  glBindVertexArray (vao[0]);
+  //Set 0 as the current buffer to be used by binding it
+  glBindBuffer (GL_ARRAY_BUFFER, vbo[0]);
+
+  // Call the sphere function
+  Lat = tesselation;
+  Long = tesselation;
+  sphere(Radius, Lat, Long);
+  
+  //Copy the points into the current buffer
+  glBufferData (GL_ARRAY_BUFFER, sizeof (v_positions) + sizeof(v_colors) + sizeof(v_normals), NULL, GL_STATIC_DRAW);
+  glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(v_positions), v_positions );
+  glBufferSubData( GL_ARRAY_BUFFER, sizeof(v_positions), sizeof(v_colors), v_colors );
+  glBufferSubData( GL_ARRAY_BUFFER, sizeof(v_colors)+sizeof(v_positions), sizeof(v_normals), v_normals );
+  // set up vertex array
+
+  glEnableVertexAttribArray( vPosition );
+  glVertexAttribPointer( vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0) );
+  
+  glEnableVertexAttribArray( vColor );
+  glVertexAttribPointer( vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(v_positions)) );
+
+  glEnableVertexAttribArray( vNormal );
+  glVertexAttribPointer( vNormal, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(v_positions) + sizeof(v_colors)) );
+
+  // For The Wireframe too ... --------
+
+  //Set 1 as the current array to be used by binding it
+  glBindVertexArray (vao[1]);
+  //Set 1 as the current buffer to be used by binding it
+  glBindBuffer (GL_ARRAY_BUFFER, vbo[1]);
+
+    //Copy the points into the current buffer
+  glBufferData (GL_ARRAY_BUFFER, sizeof (w_positions) + sizeof(w_colors) + sizeof(w_normals), NULL, GL_STATIC_DRAW);
+  glBufferSubData( GL_ARRAY_BUFFER, 0, sizeof(w_positions), w_positions );
+  glBufferSubData( GL_ARRAY_BUFFER, sizeof(w_positions), sizeof(w_colors), w_colors );
+  glBufferSubData( GL_ARRAY_BUFFER, sizeof(w_colors)+sizeof(w_positions), sizeof(w_normals), w_normals );
+  
+  // set up vertex array
+
+  glEnableVertexAttribArray( vPosition );
+  glVertexAttribPointer( vPosition, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0) );
+  
+  glEnableVertexAttribArray( vColor );
+  glVertexAttribPointer( vColor, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(w_positions)) );
+
+  glEnableVertexAttribArray( vNormal );
+  glVertexAttribPointer( vNormal, 4, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(sizeof(w_positions) + sizeof(w_colors)) );
+
+
+}
+
+void renderGL(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  rotation_matrix = glm::rotate(glm::mat4(1.0f), glm::radians(xrot), glm::vec3(1.0f,0.0f,0.0f));
+  rotation_matrix = glm::rotate(rotation_matrix, glm::radians(yrot), glm::vec3(0.0f,1.0f,0.0f));
+  rotation_matrix = glm::rotate(rotation_matrix, glm::radians(zrot), glm::vec3(0.0f,0.0f,1.0f));
+  model_matrix = rotation_matrix;
+
+  //Creating the lookat and the up vectors for the camera
+  c_rotation_matrix = glm::rotate(glm::mat4(1.0f), glm::radians(c_xrot), glm::vec3(1.0f,0.0f,0.0f));
+  c_rotation_matrix = glm::rotate(c_rotation_matrix, glm::radians(c_yrot), glm::vec3(0.0f,1.0f,0.0f));
+  c_rotation_matrix = glm::rotate(c_rotation_matrix, glm::radians(c_zrot), glm::vec3(0.0f,0.0f,1.0f));
+
+  glm::vec4 c_pos = glm::vec4(c_xpos,c_ypos,c_zpos, 1.0)*c_rotation_matrix;
+  glm::vec4 c_up = glm::vec4(c_up_x,c_up_y,c_up_z, 1.0)*c_rotation_matrix;
+  //Creating the lookat matrix
+  lookat_matrix = glm::lookAt(glm::vec3(c_pos),glm::vec3(0.0),glm::vec3(c_up));
+
+  //creating the projection matrix
+ 
+  projection_matrix = glm::frustum(-1.0, 1.0, -1.0, 1.0, 1.0, 5.0);
+
+  view_matrix = projection_matrix*lookat_matrix;
+
+  glUniformMatrix4fv(viewMatrix, 1, GL_FALSE, glm::value_ptr(view_matrix));
+
+  if(wireframe)
+    {
+      // Drawing a Wireframe for SPHERE
+      modelview_matrix = view_matrix*model_matrix;
+      glUniformMatrix4fv(uModelViewMatrix, 1, GL_FALSE, glm::value_ptr(modelview_matrix));
+      glBindVertexArray (vao[1]);
+      glDrawArrays(GL_LINE_STRIP,0,num_vertices);
+    }
+vector<std::string> earth
+    {"A.png",
+        "B.png",
+        "C.png",
+        "D.png",
+       "E.png",
+      "F.png"
+    };
+    unsigned int cubemap2Texture = loadCubemap(earth);
+
+  // Draw the sphere
+  modelview_matrix = view_matrix*model_matrix;
+  glUniformMatrix4fv(uModelViewMatrix, 1, GL_FALSE, glm::value_ptr(modelview_matrix));
+  normal_matrix = glm::transpose (glm::inverse(glm::mat3(modelview_matrix)));
+  glUniformMatrix3fv(normalMatrix, 1, GL_FALSE, glm::value_ptr(normal_matrix));
+glActiveTexture(GL_TEXTURE0);    
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemap2Texture);
+  glBindVertexArray (vao[0]);
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, num_vertices);
+  
+}
+
+int main(int argc, char** argv)
+{  //! The pointer to the GLFW window
+  GLFWwindow* window;
+
+  //! Setting up the GLFW Error callback
+  glfwSetErrorCallback(csX75::error_callback);
+
+  //! Initialize GLFW
+  if (!glfwInit())
+    return -1;
+  if(argc > 1)
+    tesselation = atoi(argv[1]);
+  //We want OpenGL 4.0
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  //This is for MacOSX - can be omitted otherwise
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
+  //We don't want the old OpenGL 
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); 
+
+  //! Create a windowed mode window and its OpenGL context
+  window = glfwCreateWindow(800, 600, "CS475/CS675 Tutorial 5: Shading a Sphere", NULL, NULL);
+  if (!window)
+    {
+      glfwTerminate();
+      return -1;
+    }
+  
+  //! Make the window's context current 
+  glfwMakeContextCurrent(window);
+ glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+  //Initialize GLEW
+  //Turn this on to get Shader based OpenGL
+  glewExperimental = GL_TRUE;
+  GLenum err = glewInit();
+  if (GLEW_OK != err)
+    {
+      //Problem: glewInit failed, something is seriously wrong.
+      std::cerr<<"GLEW Init Failed : %s"<<std::endl;
+    }
+
+
+
+
+
+// configure global opengl state
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // build and compile shaders
+    // -------------------------
+    Shader shader("cubemap.vs", "cubemap.fs");
+    Shader skyboxShader("6.1.skybox.vs", "6.1.skybox.fs");
+
+ float skyboxVertices[] = {
+        // positions          
+        -1.0f,  1.0f, -1.0f,
+        -1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f, -1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+
+        -1.0f, -1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+        -1.0f, -1.0f,  1.0f,
+
+        -1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f, -1.0f,
+         1.0f,  1.0f,  1.0f,
+         1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f,  1.0f,
+        -1.0f,  1.0f, -1.0f,
+
+        -1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f, -1.0f,
+         1.0f, -1.0f, -1.0f,
+        -1.0f, -1.0f,  1.0f,
+         1.0f, -1.0f,  1.0f,
+    };
+
+
+vector<std::string> faces
+    {"top.png",
+        "bottom.png",
+        "right.png",
+        "left.png",
+       "front.png",
+      "back.png"
+    };
+
+
+
+    unsigned int cubemapTexture = loadCubemap(faces);
+
+ 
+
+// skybox VAO
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+
+shader.use();
+    shader.setInt("cubemap", 0);
+skyboxShader.use();
+    skyboxShader.setInt("skybox", 0);
+
+
+
+  //Keyboard Callback
+  glfwSetKeyCallback(window, csX75::key_callback);
+  //Framebuffer resize callback
+  glfwSetFramebufferSizeCallback(window, csX75::framebuffer_size_callback);
+
+  // Ensure we can capture the escape key being pressed below
+  glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+  //Initialize GL state
+  csX75::initGL();
+  initBuffersGL();
+
+
+
+
+  // Loop until the user closes the window
+  while (glfwWindowShouldClose(window) == 0)
+    {           
+ // per-frame time logic
+        // --------------------
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // input
+        // -----
+        processInput(window);
+
+
+ glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+
+shader.use(); 
+glm::mat4 model = glm::mat4(1.0f);
+ glm::mat4 view = camera.GetViewMatrix();
+//view = glm::mat4(glm::mat3(camera.GetViewMatrix())); // remove translation from the view matrix 
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 1.0f, 50.0f);
+
+
+
+ 
+
+
+shader.setMat4("view", view);
+ shader.setMat4("projection", projection);
+       
+ glDepthFunc(GL_LEQUAL);
+            skyboxShader.use();
+
+      //view = glm::mat4(glm::mat3(camera.GetViewMatrix()));
+
+        skyboxShader.setMat4("view", view);
+        skyboxShader.setMat4("projection", projection);
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+
+
+
+
+
+
+  
+
+     
+
+      // Swap front and back buffers
+      glfwSwapBuffers(window);
+      
+      // Poll for and process events
+      glfwPollEvents();
+}
+  glfwTerminate();
+  return 0;
+};
+
+
+ 
+
+
+
+
+unsigned int loadCubemap(vector<std::string> faces)
+{
+    unsigned int textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+
+    int width, height, nrChannels;
+    for (unsigned int i = 0; i < faces.size(); i++)
+    {
+        unsigned char *data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
+        if (data)
+        {
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Cubemap texture failed to load at path: " << faces[i] << std::endl;
+            stbi_image_free(data);
+        }
+    }
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+    return textureID;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.ProcessKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    camera.ProcessMouseScroll(yoffset);
+}
+
+
+//-------------------------------------------------------------------------
+
